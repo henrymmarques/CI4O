af2cb686d276a966ca009f7a280d0ff37b571b66
hmpms-iscte <hmpms@iscte-iul.pt>
2023-05-11 18:34:02 +0100

    arthmetic crossover

--- crossover_TEST.py
+++ crossover_TEST.py
@@ -1,5 +1,6 @@
 import random 
 from data.data import nutrients, data
+import numpy as np
 
 
 
@@ -20,6 +21,24 @@ def two_point_crossover(parent1, parent2):
     return offspring1, offspring2
 
 
+def arithmetic_xo(p1, p2):
+    """Implementation of arithmetic crossover/geometric crossover with constant alpha.
+
+    Args:
+        p1 (Individual): First parent for crossover.
+        p2 (Individual): Second parent for crossover.
+
+    Returns:
+        Individuals: Two offspring, resulting from the crossover.
+    """
+    alpha = random.uniform(0, 1)
+    o1 = [None] * len(p1)
+    o2 = [None] * len(p1)
+    for i in range(len(p1)):
+        o1[i] = round(p1[i] * alpha + (1-alpha) * p2[i], 3)
+        o2[i] = round(p2[i] * alpha + (1-alpha) * p1[i], 3)
+    return o1, o2
+
 
 if __name__ == '__main__':
     # Define the indices of the two parents
@@ -31,7 +50,8 @@ if __name__ == '__main__':
     parent2 = data[parent2_index][2:]
 
     # Perform the two-point crossover
-    offspring1, offspring2 = two_point_crossover(parent1, parent2)
+    # offspring1, offspring2 = two_point_crossover(parent1, parent2)
+    offspring1, offspring2 = arithmetic_xo(parent1, parent2)
 
     # Print the parents and child
     print(f"Parent 1: {data[parent1_index]}")
