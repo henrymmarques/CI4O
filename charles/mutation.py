4b699705012409b3b96a7839aff5c3a1cd6af9d5
hmpms-iscte <hmpms@iscte-iul.pt>
2023-05-12 10:04:03 +0100

    random mutation

--- charles/mutation.py
+++ charles/mutation.py
@@ -1,32 +1,29 @@
-from random import randint
+import random
 
 
-def binary_mutation(individual):
-    """Binary mutation for a GA individual
+####   ESTA MUTATION FALTA ADICIONAR UMA PERCENTAGEM PARA MUDAR A QUANTIDADE ############
+def random_mutation(individual, mutation_rate=0.1):
+    # Make a copy of the individual to avoid changing the original
+    mutated_individual = individual.copy()
+    
+    # # Mutate the quantity by a random percentage
+    # quantity_mutated = round(mutated_individual[2] * (1 + random.uniform(-mutation_rate, mutation_rate)), 1)
+    # mutated_individual[2] = quantity_mutated
+    
+    # Mutate the nutrient values by adding a random value
+    for i in range(3, len(mutated_individual)):
+        nutrient_mutated = round(mutated_individual[i] + random.uniform(-mutation_rate, mutation_rate), 1)
+        mutated_individual[i] = nutrient_mutated
+        
+    return mutated_individual
 
-    Args:
-        individual (Individual): A GA individual from charles.py
 
-    Raises:
-        Exception: When individual is not binary encoded.py
 
-    Returns:
-        Individual: Mutated Individual
-    """
-    mut_index = randint(0, len(individual) - 1)
-
-    if individual[mut_index] == 0:
-        individual[mut_index] = 1
-    elif individual[mut_index] == 1:
-        individual[mut_index] = 0
-    else:
-        raise Exception(
-            f"Trying to do binary mutation on {individual}. But it's not binary.")
-    print(individual)
-    return individual
 
 
 if __name__ == '__main__':
-    test = [0,0,0,0,0,0,0,0]
-    test = binary_mutation(test)
+    data= ['Wheat Flour (Enriched)', '10 lb.', 36, 44.7, 1411, 2, 365, 0, 55.4, 33.3, 441, 0]
+
+    new_individual = random_mutation(data)
+    print(new_individual)
 
